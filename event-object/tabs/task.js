5e699a9aa8c0c3099a6e9a1b1417b66aa0447941
Smyslova Natalia <madlynx@lenta.ru>
2022-06-10 21:30:46 +0300

    first commit

---
+++ event-object/tabs/task.js
@@ -0,0 +1,30 @@
+// Процесс реализации
+// Зарегистрируйте обработчики событий на переключателях вкладок
+// Сделайте акцент на том, что таких механизмов переключения может быть несколько на странице
+
+// Советы
+// Для удобного нахождения индекса вкладки, можно использовать метод indexOf 
+// в найденном массиве вкладок .tab. Найденный индекс можно использовать для показа нужного содержимого .tab__c
+
+// Для того, чтобы вкладка была выбранной, у элемента с классом tab должен быть класс tab_active:
+// Для того, чтобы показывалось верное содержимое, у элемента с классом tab__content должен быть класс tab__content_active:
+// При переключении вкладки, нужно поставить эти два активных класса соответствующим элементам.
+
+const tab = document.querySelectorAll("div.tab");
+const arrTab = Array.from(tab);
+
+const content = document.querySelectorAll("div.tab__content");
+const arrContent = Array.from(content);
+
+for (let i = 0; i < arrTab.length; i++) {
+    arrTab[i].onclick = function () {
+        arrTab[i].classname = "tab_active";
+        let number = arrTab.indexOf(arrTab[i]);
+        arrContent.forEach(function (element, index) {
+            if (index != number) {
+                element.className = "tab__content";
+            }
+            else { element.className = "tab__content_active"; }
+        })
+    };
+};
