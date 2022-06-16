c904613beaa03e64f1241399d4e1e5ed55893228
Smyslova Natalia <madlynx@lenta.ru>
2022-06-16 16:55:15 +0300

    first commit

---
+++ dom/ads/task.js
@@ -0,0 +1,24 @@
+// Необходимо реализовать механизм смены текстовых объявлений. Каждое объявление меняется через 1 секунду.
+
+// Для того, чтобы задать текстовый элемент активным, установите у него класс rotator__case_active:
+
+// Процесс реализации
+
+// Каждую секунду меняйте класс с одного элемента на другой
+// Сделайте акцент на том, чтобы на странице можно было использовать несколько ротаторов одновременно
+// Смена текстовых блоков должна быть бесконечной.
+// Подумайте, как из setInterval сделать бесконечный цикл
+
+const rotator = document.querySelectorAll("span.rotator__case");
+
+setInterval(() => {
+    let rotatorActive = document.querySelector("span.rotator__case_active");
+    let nextElement = rotatorActive.nextElementSibling;
+    rotatorActive.classList.toggle("rotator__case_active");
+    if (nextElement == null) {
+        document.querySelector("span.rotator__case:first-child").classList.toggle("rotator__case_active");
+    }
+    else {
+        nextElement.classList.toggle("rotator__case_active");
+    }
+}, 1000);
