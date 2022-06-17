d5d3ca6291e096b7f9dfeebbee24e1b4bb43667f
Smyslova Natalia <madlynx@lenta.ru>
2022-06-17 14:33:58 +0300

    second commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -12,73 +12,24 @@
 const links = document.querySelectorAll(".menu_main > li > a.menu__link");
 console.log(links);
 
-for(let i = 0; i < links.length; ++i){
+for (let i = 0; i < links.length; ++i) {
     console.log(links[i]);
-    links[i].onclick = function(){
+    links[i].onclick = function () {
         // console.log(this.parentElement.querySelectorAll("ul").length);
         console.log(this);
-        if(this.parentElement.querySelectorAll("ul").length){
+        if (this.parentElement.querySelectorAll("ul").length) {
+            let isActive = this.parentElement.querySelector("ul").classList.contains('menu_active');
+            console.log(isActive);
             // <ul class="menu menu_sub menu_active"> => <ul class="menu menu_sub">
-            document.querySelectorAll(".menu_main ul.menu.menu_sub.menu_active").forEach(function(item){
-                item.className = "menu menu_sub";
-            }); 
-            this.parentElement.querySelector("ul").className = "menu menu_sub menu_active";
+            document.querySelectorAll(".menu_main ul.menu.menu_sub.menu_active").forEach(function (item) {
+                // item.className = "menu menu_sub";
+                item.classList.remove("menu_active");
+            });
+            if (!isActive)
+                this.parentElement.querySelector("ul").classList.add("menu_active");
+            // this.parentElement.querySelector("ul").classList.toggle("menu_active");
             return false;
         }
     };
 
 }
-
-// const arr = Array.from(link);
-// console.log(arr);
-
-// console.log(arr[1].closest("ul").querySelector("ul.menu.menu_sub").className);
-
-
-
-// const ul = document.querySelectorAll("ul.menu.menu_sub");
-// console.log(ul);
-
-
-// // ВАРИАНТ ВТОРОЙ
-
-// for (let i = 1; i < arr.length; i++) {
-//     //Получили все элементы (2) menu.menu_sub
-//     console.log(arr[i].closest("ul").querySelectorAll("ul.menu.menu_sub"));
-//     //так работает только у ближайшего элемента
-//     console.log(arr[i].closest("ul").querySelector("ul.menu.menu_sub").className);
-    
-//     if (arr[i].closest("ul").querySelectorAll("ul.menu.menu_sub") != null) {
-//         arr[i].onclick = function () {
-//             let test = arr[i].closest("ul").querySelector("ul.menu.menu_sub");
-//             console.log(test);
-//             // test[i].className = "menu menu_sub menu_active";
-//             // return false
-
-
-
-//             // menu.querySelector("ul.menu.menu_sub").className = "menu menu_sub menu_active";
-//             // arr[i].closest("ul.menu.menu_sub").className = "menu menu_sub menu_active";
-//             // return false
-//         }
-//         break
-//     }
-// }
-// // }
-
-
-
-// ВАРИАНТ ПЕРВЫЙ
-
-
-// for (let i = 1; i < arr.length; i++) {
-
-//     if (arr[i].closest("ul") != null) {
-//         let menu = arr[i].closest("ul");
-//         arr[i].onclick = function () {
-//             menu.className = "menu menu_sub menu_active";
-//             console.log(menu.className);
-//             return false
-//         }
-//     }
-// }
