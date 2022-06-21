36579e3c39c80e0261c84509f385e21984a09725
Smyslova Natalia <madlynx@lenta.ru>
2022-06-21 17:18:09 +0300

    second commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -16,15 +16,25 @@ const arrTab = Array.from(tab);
 const content = document.querySelectorAll("div.tab__content");
 const arrContent = Array.from(content);
 
+
 for (let i = 0; i < arrTab.length; i++) {
     arrTab[i].onclick = function () {
-        arrTab[i].classname = "tab_active";
+        if (arrTab[i].classList.contains("tab")) {
+            let activeTab = document.querySelector("div.tab_active");
+            activeTab.classList.remove("tab_active");
+            arrTab[i].classList.add("tab_active");
+        };
+
         let number = arrTab.indexOf(arrTab[i]);
+        console.log(number);
         arrContent.forEach(function (element, index) {
+            console.log(index);
             if (index != number) {
                 element.className = "tab__content";
             }
-            else { element.className = "tab__content_active"; }
+            else {
+                element.className = "tab__content_active";
+            }
         })
     };
 };
