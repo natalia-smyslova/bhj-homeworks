798ab3852be45057d454b93a230dbe6e51fa2f95
Smyslova Natalia <madlynx@lenta.ru>
2022-07-07 19:42:59 +0300

    second commit

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,13 +1,3 @@
-// Процесс реализации
-// Загрузите форму через AJAX по адресу https://netology-slow-rest.herokuapp.com/upload.php
-
-
-//ВОПРОСЫ:
-// Почему объект formData остается пустым, но при этом по запросу get(name) показывает файл из импута?
-// Как понять, что объект formData передался в send? 
-// Как должен отобразиться размер файла в onprogress.total?
-
-
 const progress = document.getElementById('progress');
 
 let xhr = new XMLHttpRequest();
@@ -17,41 +7,20 @@ let input = document.getElementById("file");
 let button = document.getElementById("send");
 button.onclick = function () {
 
-    // let files = input.files;
-    // console.log(files);
-    // let file = files[0];
-    // console.log(file);
-    // let name = input.name;
-    // console.log(name);
 
     let formData = new FormData(document.forms.form);
-    // let formData = new FormData();
-    // formData.append('test', 123);
-    // formData.append(name, file);
-
-    // console.log(formData.get(name));
-    // console.log(formData.entries());
-
     for (const pair of formData.entries()) {
         console.log(`${pair[0]}, ${pair[1]}`);
     }
 
     xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php", true);
-    // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.upload.onprogress = function (event) {
-        // console.log(event.loaded / event.total * 100);
         progress.value = event.loaded / event.total;
     }
-    xhr.onprogress = function (event) {
-        // console.log(event);
-        progress.value = event.loaded / (1024 * 1024 * 100);
-    }
+    // xhr.onprogress = function (event) {
+    //     progress.value = event.loaded / (1024 * 1024 * 100);
+    // }
     xhr.send(formData);
-    // xhr.upload.onprogress
 
     return false
 };
-
-
-
-// Отслеживайте событие изменения загрузки и в соответствии с ним корректируйте прогресс
